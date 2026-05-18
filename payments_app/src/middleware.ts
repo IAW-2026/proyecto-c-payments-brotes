423fe436813178c2dcca5381cf98f39a215aaa81
Sebastian Sandoval <sebassantisandoval@gmail.com>
2026-05-18 12:24:36 -0300

    + actualización de middleware.ts protege rutas api/

--- payments_app/src/middleware.ts
+++ payments_app/src/middleware.ts
@@ -1,13 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 //protección de rutas con clerk
 const isProtectedRoute = createRouteMatcher([
   "/(buyer)(.*)",
   "/(seller)(.*)",
   "/(admin)(.*)",
-  "/api/(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { pathname } = req.nextUrl;
+  if (pathname.startsWith("/api")) {
+    const authHeader = req.headers.get("autorization");
+    const token = authHeader?.replace("Beares", "");
+
+    if (!token || token !== process.env.SERVICE_API_KEY) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    return NextResponse.next();
+  }
+
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
