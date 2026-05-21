bfe3bc14e17244ed6d54e88d2160b7ae31158084
Sebastian Sandoval <sebassantisandoval@gmail.com>
2026-05-20 22:26:28 -0300

    *arreglo: las rutas no estaban correctamente protegidas

--- payments_app/src/proxy.ts
+++ payments_app/src/proxy.ts
@@ -1,21 +1,31 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
-
+/*
 const isProtectedRoute = createRouteMatcher([
   "/(buyer)(.*)",
   "/(seller)(.*)",
   "/(admin)(.*)",
 ]);
-
+ */
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-
+/*
 const routeRoleMap: { matcher: (req: NextRequest) => boolean; role: string }[] =
   [
     { matcher: createRouteMatcher(["/(buyer)(.*)"]), role: "buyer" },
     { matcher: createRouteMatcher(["/(seller)(.*)"]), role: "seller" },
     { matcher: createRouteMatcher(["/(admin)(.*)"]), role: "admin" },
-  ];
+    ]; */
+const isProtectedRoute = createRouteMatcher([
+  "/payments(.*)",
+  "/payouts(.*)",
+  "/dashboard(.*)",
+]);
 
+const routeRoleMap = [
+  { matcher: createRouteMatcher(["/payments(.*)"]), role: "buyer" },
+  { matcher: createRouteMatcher(["/payouts(.*)"]), role: "seller" },
+  { matcher: createRouteMatcher(["/dashboard(.*)"]), role: "admin" },
+];
 export default clerkMiddleware(async (auth, req: NextRequest) => {
   const { pathname } = req.nextUrl;
 
@@ -31,14 +41,15 @@ export default clerkMiddleware(async (auth, req: NextRequest) => {
 
   if (isPublicRoute(req)) return NextResponse.next();
 
-  if (isPublicRoute(req)) return NextResponse.next();
-
   if (isProtectedRoute(req)) {
     // 1. Verificar autenticación
     const { userId, sessionClaims } = await auth.protect();
-
+    /*    console.log(
+      "sessionClaims completo:",
+      JSON.stringify(sessionClaims, null, 2),
+    ); */
     // 2. Leer rol desde publicMetadata
-    const userRole = (sessionClaims?.metadata as { role?: string })?.role;
+    const userRole = (sessionClaims?.publicMetadata as { role?: string })?.role;
 
     // 3. Verificar que el rol coincide con la ruta
     const requiredRole = routeRoleMap.find(({ matcher }) => matcher(req))?.role;
